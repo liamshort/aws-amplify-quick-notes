cfa66ba8a09fb957ce57d97a8d47143e9a9d912a
LiamShort <liamshort@pm.me>
2020-10-31 13:37:22 +0000

    Updated app home header

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
     <Screens />
   ) : (
     <>
-      <Title>Quick Notes</Title>
+      <Title>Liam Short Notes R US</Title>
       <Authenticator
         onStateChange={authState => {
           if (authState === "signedIn") {
